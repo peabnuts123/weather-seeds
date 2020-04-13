cd3b362c136f4d3b6a8084823f0343b42e64ec11
Jeff Andrews <jeff.andrews@alphero.com>
2020-04-13 21:27:30 +1200

    Remove extraneous links from header

--- www/client/src/components/header.tsx
+++ www/client/src/components/header.tsx
@@ -1,15 +1,13 @@
-import { FunctionalComponent, h } from "preact";
+import { h } from "preact";
 import { Link } from "preact-router/match";
 
-const Header: FunctionalComponent = () => {
+const Header = () => {
   return (
     <header class="Header">
       <h1>Weather Seeds</h1>
 
       <nav>
         <Link activeClassName="active" href="/">Home</Link>
-        <Link activeClassName="active" href="/about">About</Link>
-        <Link activeClassName="active" href="/limes">Limes</Link>
       </nav>
     </header>
   );
